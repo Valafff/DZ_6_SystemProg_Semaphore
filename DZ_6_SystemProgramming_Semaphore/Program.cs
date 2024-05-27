f069c5c835a3142a9338b963083573af16c11555
Vetal <vvvv@vvvv.vv>
2024-05-27 20:34:01 +0300

    Add project files.

---
+++ DZ_6_SystemProgramming_Semaphore/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_6_SystemProgramming_Semaphore
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+	}
+}
